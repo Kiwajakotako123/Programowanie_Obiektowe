d4a2b4643bbd3d4bae2306ceb9e47228c84ade9c
Kiwajakotako123 <98175143+Kiwajakotako123@users.noreply.github.com>
2022-10-18 17:35:26 +0200

    Add files via upload

---
+++ zad_klasy.py
@@ -0,0 +1,112 @@
+#zad1
+class Punkt(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @staticmethod
+    def distance(Punkt1, Punkt2):
+        dlu = ((Punkt1.a - Punkt2.a) ** 2 + (Punkt1.b - Punkt2.b) ** 2)**(1/2)
+        return dlu
+
+
+
+Punkt1 = Punkt(1,3)
+NazwanyPunkt = Punkt(3,7)
+print(Punkt.distance(Punkt1,NazwanyPunkt))
+
+#zad2
+
+class Adres(object):
+    def __init__(self, ulica, nr_dom, miasto,kod_poc, nr_mie=None):
+        self.ulica = ulica
+        self.nr_dom = nr_dom
+        self.miasto = miasto
+        self.kod_poc = kod_poc
+
+        if nr_mie is not None:
+            self.nr_mie = nr_mie
+
+
+    def show(self) -> None:
+        if hasattr(self, 'nr_mie'):
+            print(f'{self.ulica} {self.nr_dom}/{self.nr_mie}')
+            print(f'{self.kod_poc} {self.miasto}')
+            return None
+        print(f'{self.ulica} {self.nr_dom}')
+        print(f'{self.kod_poc} {self.miasto}')
+
+    def comes_before(self, other):
+        for i in range(len(self.kod_poc)):
+            if(self.kod_poc[i] == other.kod_poc[i]):
+                continue
+            if(self.kod_poc[i] < other.kod_poc[i]):
+                return True
+            return False
+
+adres1 = Adres("Złota", "4", "Morąg", "14-300", "5")
+
+adres1.show()
+#zad3
+
+
+class SodaCan:
+    def __init(self, h, r):
+        self.h = h
+        self.r = r
+
+    def get_volume(self) -> float:
+        return math.pi * self.r * self.r * self.h
+
+    def get_surface(self) -> float:
+        return 2 * math.pi * self.r * (self.r * self.h)
+
+#zad4
+
+class Car:
+    def __init__(self, efficiency: float, max_fuel: float):
+        self.efficiency = efficiency
+        self.max_fuel = max_fuel
+        self.current_fuel = 0
+
+    def drive(self, distance: float) -> None:
+        fuel_to_burn = distance / self.efficiency
+        if fuel_to_burn > self.current_fuel:
+            print('Nie ma tyle paliwa w baku lol.')
+            return None
+        self.current_fuel -= distance / self.efficiency
+
+    def get_fuel_level(self) -> float:
+        return self.current_fuel
+
+    def add_fuel(self, fuel_amount: float) -> None:
+        if fuel_amount + self.current_fuel > self.max_fuel:
+            print('Nie ma tyle miejsca lol.')
+            return None
+        self.current_fuel += fuel_amount
+
+#zad5
+
+class Student:
+    def __init__(self, imie: str, nazwisko: str, *args):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.liczba_quizuw = len(args)
+        try:
+            suma = 0
+            for i in args:
+                suma += i
+            self.srednia_quizuw = suma / self.liczba_quizuw
+        except ZeroDivisionError:
+            self.srednia_quizuw = 0
+
+
+    def add_quiz(self, score):
+        self.srednia_quizuw = ((self.srednia_quizuw * self.liczba_quizuw) + score) / (self.liczba_quizuw + 1)
+        self.liczba_quizuw += 1
+
+    def get_total_score(self):
+        return self.srednia_quizuw * self.liczba_quizuw
+
+    def get_average_score(self):
+        return self.srednia_quizuw
